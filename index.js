e3dcd9dcaddfd749af04496065b558c9b8222b62
WrittenInnaCode <writteninnacode@gmail.com>
2022-09-06 00:46:52 -0700

    wrote all functions and question prompts

--- index.js
+++ index.js
@@ -1,13 +1,74 @@
 // TODO: Include packages needed for this application
+const path = require("path");
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'GitHub',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Please write a short description of your project:',
+        name: 'description',
+    },
+    {
+        type: 'list',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+        name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to install dependencies?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to run tests?',
+        name: 'tests',
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about using the repo?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about contributing to the repo?',
+        name: 'contribution',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        writeToFile('./dist/README.md', generateMarkdown(answers))
+        console.log('Your file is saved!');
+    })
+        .catch((err) => {
+            console.log(err);
+        });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,66 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "None") {
+    return `<img src="https://img.shields.io/badge/license-${license}-blue.svg" alt="GitHub License">`
+  }
+  return ''
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "None") {
+    return (`* [Licenses](#license)`)
+  }
+  return ''
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return (`## License \n
+This project is licensed under the ${license} license.`)
+  }
+  return ''
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)} 
 
+  ## Table of Contents:
+  * [Description](#description)
+  ${renderLicenseLink(data.license)} 
+  * [Installation](#installation)
+  * [Test](#test)
+  * [Usage](#usage)
+  * [Contribution](#contribution)
+  * [Questions](#questions)
+
+  ## Description:
+  ${data.description}
+  
+  ${renderLicenseSection(data.license)} 
+
+  ## Installation:
+  ${data.installation}
+
+  ## Test:
+  ${data.tests}
+
+  ## Usage:
+  ${data.usage}
+
+  ## Contribution:
+  ${data.contribution}
+
+## Questions:
+If you have any questions, please contact me. \n
+My email address is ${data.email} \n
+To find my GitHub page, visit [https://github.com/${data.GitHub}](https://github.com/${data.GitHub})
 `;
 }
 
